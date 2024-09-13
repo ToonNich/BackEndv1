6ecd5f98d38b5ac33ebd435848b87917a90357d1
Nichapat <Nichapat_Padung@cmu.ac.th>
2024-09-13 10:18:46 +0700

    Lab7

---
+++ 331-backend/src/test/java/se331/lab/ApplicationTests.java
@@ -0,0 +1,17 @@
+package se331.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
